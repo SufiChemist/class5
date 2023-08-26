efe5b2512515cbf903aa86eff56e215dd462ca5a
SufiChemist <chemistmr4@gmail.com>
2023-08-26 15:29:21 +0500

    asignment

---
+++ class-5.js
@@ -0,0 +1,18 @@
+"use strict";
+// var i :number =1;
+// while( i<20){
+//     console.log("sufi", i)
+//     i =i +1
+// }
+// for loop
+let sum = 0;
+Array.list;
+number[] = [];
+for (let i = 0; i < 100; i++) {
+    if (i % 2 == 0) {
+        sum = sum + i;
+        list, push(i);
+        console.log("sufi", i);
+    }
+}
+console.log("sum", sum);
